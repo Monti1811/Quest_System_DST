029c615386df5a83d1a655117225b2b3bbc8bf85
Timon Doerzapf <s9tidoer@stud.uni-saarland.de>
2023-07-29 09:06:39 +0200

    First version of adding draggable quest button

--- scripts/widgets/button_questlog.lua
+++ scripts/widgets/button_questlog.lua
@@ -1,19 +1,27 @@
 local Widget = require "widgets/widget"
 local ImageButton = require "widgets/imagebutton"
 
-local Button_QuestLog = Class(Widget, function(self, owner,background)
-	Widget._ctor(self,"Button_QuestLog")
+local Button_QuestLog = Class(ImageButton, function(self, owner,background)
+	--ImageButton._ctor(self,"Button_QuestLog")
+
+	if background then
+		ImageButton._ctor(self, "images/global_redux.xml", "button_carny_square_normal.tex", "button_carny_square_hover.tex", "button_carny_square_disabled.tex", "button_carny_square_down.tex")
+		self.button_img = self:AddChild(Image("images/images_quest_system.xml","quest_log.tex"))
+		self:SetScale(1)
+		self:SetDraggable(false)
+		self:SetOnDragFinish(nil)
+	else
+		ImageButton._ctor(self, "images/images_quest_system.xml", "quest_log.tex", nil, nil, nil, nil, {1,1}, {0,0})
+		self:SetImageNormalColour(1, 1, 1, 1)
+		self.move_on_click = false
+		self.drag_tolerance = 4
+		self:SetDraggable(true)
+		self:SetOnDragFinish(nil)
+	end
 
 	self.owner = owner
-	self.root = self:AddChild(Widget("ROOT"))
-	if background then
-		self.button = self:AddChild(ImageButton("images/global_redux.xml", "button_carny_square_normal.tex", "button_carny_square_hover.tex", "button_carny_square_disabled.tex", "button_carny_square_down.tex"))
-		self.button_img = self.button:AddChild(Image("images/images_quest_system.xml","quest_log.tex"))
-		self.button:SetScale(1)
-	else
-		self.button = self:AddChild(ImageButton("images/images_quest_system.xml", "quest_log.tex", nil, nil, nil, nil, {1,1}, {0,0}))
-	end
-	self.button:SetOnClick(function()
+
+	self.onclick2 = function()
 		local screen = TheFrontEnd:GetActiveScreen()
         if not screen or not screen.name then return true end
         if screen.name:find("HUD") then
@@ -28,14 +36,172 @@ local Button_QuestLog = Class(Widget, function(self, owner,background)
                 screen:OnClose()
             end
 		end
-	end)
-	self.button:SetTooltip(STRINGS.QUEST_COMPONENT.QUEST_LOG.BUTTON)
-	local scale = 64 / math.max(self.button:GetSize())
-	self.button:SetScale(scale, scale, scale)
-	local w, h = self.button:GetSize()
+	end
+	self:SetTooltip(STRINGS.QUEST_COMPONENT.QUEST_LOG.BUTTON)
+	local scale = 64 / math.max(self:GetSize())
+	self:SetScale(scale, scale, scale)
+	local w, h = self:GetSize()
 	self.width = w * scale
 	self.height = h * scale
 
+
+
 end)
+
+function Button_QuestLog:SetDraggable(bool)
+	self.draggable = bool
+	if self.draggable then
+		self:SetOnDown(function()
+			self:BeginDrag()
+		end)
+		self:SetWhileDown(function()
+			self:DoDrag()
+		end)
+		self.onclick = function(...)
+			self:EndDrag()
+			if self.onclick2 then
+				return self.onclick2(...)
+			end
+		end
+	else
+		self:SetOnDown(nil)
+		self:SetWhileDown(nil)
+		self.onclick = function(...)
+			if self.onclick2 then
+				return self.onclick2(...)
+			end
+		end
+	end
+end
+
+function Button_QuestLog:SetOnDragFinish(fn)
+	self.ondragfinish = fn
+end
+
+--- Overwrites the normal SetOnClick because we need to have a separate onclick to stop dragging, and to handle actual clicks.
+function Button_QuestLog:SetOnClick(fn)
+	self.onclick2 = fn
+end
+
+function Button_QuestLog:OnGainFocus()
+	--devprint("gained focus")
+	return ImageButton.OnGainFocus(self)
+end
+
+function Button_QuestLog:OnLoseFocus()
+	--devprint("lost focus")
+	if self:IsDragging() then
+		--self:EndDrag()
+
+	end
+
+	return ImageButton.OnLoseFocus(self)
+end
+
+function Button_QuestLog:HasMoved()
+	if self.drag_state == nil then
+		return false
+	end
+
+	local bx, by, bz = self.drag_state.origin:Get()
+	local x, y, z = self:GetPosition():Get()
+
+	if math.abs(x - bx) + math.abs(y - by) >= self.drag_tolerance then
+		return true
+	end
+
+	return false
+end
+
+function Button_QuestLog:IsDragging()
+	return self.drag_state ~= nil
+end
+
+function Button_QuestLog:BeginDrag()
+	if self:IsDragging() then
+		devprint("ALREADY DRAGGING")
+		return
+	end
+
+	if not TheFrontEnd.lastx or not TheFrontEnd.lasty then
+		return
+	end
+
+	TheFrontEnd:LockFocus(true)
+
+	self.o_pos = nil
+
+	self.drag_state = {
+		origin = self:GetPosition(),
+		pos = self:GetPosition(),
+		lastx = TheFrontEnd.lastx,
+		lasty = TheFrontEnd.lasty
+	}
+
+	self.image:SetScale(self.normal_scale[1], self.normal_scale[2], self.normal_scale[3])
+end
+
+function Button_QuestLog:DoDrag()
+	if not self.drag_state then
+		-- Happened once
+		return
+	end
+	local pos = self.drag_state.pos
+
+	-- lastx was nil? Which one? Frontend?
+	if not TheFrontEnd.lastx or not TheFrontEnd.lasty then
+		devprint("FRONTEND MISSING LASTS")
+		devprint(TheFrontEnd.lastx, TheFrontEnd.lasty)
+	end
+
+	if not self.drag_state.lastx or not self.drag_state.lasty then
+		devprint("STATE MISSING LASTS")
+		devprint(self.drag_state.lastx, self.drag_state.lasty)
+	end
+
+	local deltax = TheFrontEnd.lastx - self.drag_state.lastx
+	local deltay = TheFrontEnd.lasty - self.drag_state.lasty
+
+	local scale = self:GetScale()
+	local screen_width, screen_height = TheSim:GetScreenSize()
+	screen_width = screen_width / scale.x
+	screen_height = screen_height / scale.y
+
+	deltax = deltax / scale.x
+	deltay = deltay / scale.y
+
+	local nx = pos.x + deltax
+	local ny = pos.y + deltay
+
+	local a, b = self:GetSize()
+
+	nx = math.clamp(nx, -screen_width + a/2, -a/2) -- 0,0 is bottom right of screen
+	ny = math.clamp(ny, b/2, screen_height - b/2)
+
+	self.drag_state.pos = Vector3(nx, ny, pos.z)
+	self:SetPosition(self.drag_state.pos)
+
+	self.drag_state.lastx = TheFrontEnd.lastx
+	self.drag_state.lasty = TheFrontEnd.lasty
+end
+
+function Button_QuestLog:EndDrag()
+	if not self:IsDragging() then
+		devprint('\tnot dragging?')
+		return
+	end
+
+	TheFrontEnd:LockFocus(false)
+
+	if self.ondragfinish and self:HasMoved() then
+		self.ondragfinish(self.drag_state.origin, self:GetPosition())
+	end
+
+	self.drag_state = nil
+
+	if self.focus then
+		self.image:SetScale(self.focus_scale[1], self.focus_scale[2], self.focus_scale[3])
+	end
+end
 
 return Button_QuestLog
